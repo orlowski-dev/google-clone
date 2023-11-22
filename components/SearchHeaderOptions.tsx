2109c26bcf794bc73ff7390db7ae7f28739730f0
orlowski-dev <orlowski.dev@gmail.com>
2023-11-22 08:27:06 +0100

    Updated SearchHeaderOptions component

--- components/SearchHeaderOptions.tsx
+++ components/SearchHeaderOptions.tsx
@@ -5,7 +5,7 @@ import { AiOutlineSearch, AiOutlinePicture } from "react-icons/ai";
 
 export default function SearchHeaderOptions() {
   const pathname = usePathname();
-  const searchTerm = useSearchParams().get("searchTerm");
+  const searchParams = useSearchParams();
   const { push } = useRouter();
   return (
     <section className="border-b-[1px] border-b-gray-100">
@@ -14,7 +14,7 @@ export default function SearchHeaderOptions() {
           className={`text-sm inline-flex gap-3 items-center py-2 px-4 border-b-2 border-b-white  transition-colors text-gray-900 hover:text-blue-500 hover:border-b-blue-500 ${
             pathname === "/search/web" && " !text-blue-500 !border-b-blue-500"
           }`}
-          onClick={() => push("/search/web?searchTerm=" + searchTerm)}
+          onClick={() => push("/search/web?" + searchParams)}
         >
           <AiOutlineSearch />
           <span>All</span>
@@ -23,7 +23,7 @@ export default function SearchHeaderOptions() {
           className={`text-sm inline-flex gap-3 items-center py-2 px-4 border-b-2 border-b-white  transition-colors text-gray-900 hover:text-blue-500 hover:border-b-blue-500 ${
             pathname === "/search/image" && " !text-blue-500 !border-b-blue-500"
           }`}
-          onClick={() => push("/search/image?searchTerm=" + searchTerm)}
+          onClick={() => push(`/search/image?${searchParams}`)}
         >
           <AiOutlinePicture />
           <span>Images</span>
